976e32fa44317f05913b1d0b681a069a314a4175
Sarang <vvtkfkddl11@naver.com>
2022-10-11 15:24:02 +0900

    리아) 라이브러리 추가
    pod 'SnapKit', '~> 5.6.0'
    pod 'Then'
    pod 'Alamofire'
    pod 'SwiftyJSON'
    pod 'KakaoSDKCommon'  # 필수 요소를 담은 공통 모듈
    pod 'KakaoSDKAuth'  # 사용자 인증
    pod 'KakaoSDKUser'  # 카카오 로그인, 사용자 관리

---
+++ Pods/Target Support Files/Pods-ZX-Community-ZX-CommunityUITests/Pods-ZX-Community-ZX-CommunityUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZX_Community_ZX_CommunityUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ZX_Community_ZX_CommunityUITests
+@end
--- ZX-Community/ViewController.swift
+++ ZX-Community/ViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let test : String = "test"
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
